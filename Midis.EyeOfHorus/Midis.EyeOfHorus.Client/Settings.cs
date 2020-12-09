d1bbff298a22fe04afbe99467fdb27753f6b9593
Dmitrijs <dmitrijs.bruskovskis@inbox.lv>
2020-12-09 11:39:30 +0200

    Update

--- Midis.EyeOfHorus/Midis.EyeOfHorus.Client/Settings.cs
+++ Midis.EyeOfHorus/Midis.EyeOfHorus.Client/Settings.cs
@@ -24,7 +24,7 @@ namespace Midis.EyeOfHorus.Client
 
         private void Settings_Load(object sender, EventArgs e)
         {
-
+         
         }
 
         private void button2_Click(object sender, EventArgs e)
---
+++ Midis.EyeOfHorus/Midis.EyeOfHorus.ClientLibrary/Database/OperationsWithDatabase.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace Midis.EyeOfHorus.ClientLibrary.Database
+{
+    public class OperationsWithDatabase
+    {
+        //Function example
+        public static void DatabaseVersionCheck()
+        {
+            string cs = "Data Source=:memory:";
+            string stm = "SELECT SQLITE_VERSION()";
+
+            using var con = new SQLiteConnection(cs);
+            con.Open();
+
+            using var cmd = new SQLiteCommand(stm, con);
+            string version = cmd.ExecuteScalar().ToString();
+
+            Console.WriteLine($"SQLite version: {version}");
+        }
+        //Function example
+        public static void DatabaseCreation()
+        {
+            string cs = @"URI=file:C:\Users\Jano\Documents\test.db";
+
+            using var con = new SQLiteConnection(cs);
+            con.Open();
+
+            using var cmd = new SQLiteCommand(con);
+
+            cmd.CommandText = "DROP TABLE IF EXISTS cars";
+            cmd.ExecuteNonQuery();
+
+            cmd.CommandText = @"CREATE TABLE cars(id INTEGER PRIMARY KEY,
+                    name TEXT, price INT)";
+            cmd.ExecuteNonQuery();
+
+            cmd.CommandText = "INSERT INTO cars(name, price) VALUES('Audi',52642)";
+            cmd.ExecuteNonQuery();
+
+            cmd.CommandText = "INSERT INTO cars(name, price) VALUES('Mercedes',57127)";
+            cmd.ExecuteNonQuery();
+
+            cmd.CommandText = "INSERT INTO cars(name, price) VALUES('Skoda',9000)";
+            cmd.ExecuteNonQuery();
+
+            cmd.CommandText = "INSERT INTO cars(name, price) VALUES('Volvo',29000)";
+            cmd.ExecuteNonQuery();
+
+            cmd.CommandText = "INSERT INTO cars(name, price) VALUES('Bentley',350000)";
+            cmd.ExecuteNonQuery();
+
+            cmd.CommandText = "INSERT INTO cars(name, price) VALUES('Citroen',21000)";
+            cmd.ExecuteNonQuery();
+
+            cmd.CommandText = "INSERT INTO cars(name, price) VALUES('Hummer',41400)";
+            cmd.ExecuteNonQuery();
+
+            cmd.CommandText = "INSERT INTO cars(name, price) VALUES('Volkswagen',21600)";
+            cmd.ExecuteNonQuery();
+
+            Console.WriteLine("Table cars created");
+        }
+
+    }
+}
